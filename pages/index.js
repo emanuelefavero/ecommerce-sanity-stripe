c1b0a59021656c0e2b75fadbf75a4410acd29b78
emanuelefavero <emafaux89@gmail.com>
2023-03-14 20:21:00 +0100

    add css files

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <>Home</>
+  return (
+    <>
+      HeroBanner
+      <div className='products-heading'>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className='products-container'>
+        {['Product 1', 'Product 2'].map((product) => product)}
+      </div>
+      Footer
+    </>
+  )
 }
