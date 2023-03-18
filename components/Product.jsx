b1a1bcdc02df1d6d986b7ff9ae1487ef396df566
emanuelefavero <emafaux89@gmail.com>
2023-03-18 16:04:18 +0100

    add data to Product.jsx

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,3 +1,24 @@
-export default function Product() {
-  return <div>Product</div>
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+
+import { urlFor } from '@/lib/client' // used to build the sanity image url
+
+export default function Product({ product: { image, name, slug, price } }) {
+  return (
+    <div>
+      {/* the current part is a sanity specific thing that you need to add to the slug to get the correct path */}
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'>
+          <img
+            src={urlFor(image && image[0])}
+            alt='product first'
+            width={250}
+            height={250}
+          />
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>{price}</p>
+        </div>
+      </Link>
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,9 @@ export default function Home({ products, bannerData }) {
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
       <FooterBanner />
     </>
