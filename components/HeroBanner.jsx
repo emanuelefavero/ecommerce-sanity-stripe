7f6e58a1fc9b357246e34985a4448bedce64abe9
emanuelefavero <emafaux89@gmail.com>
2023-03-18 07:03:26 +0100

    add sanity setup

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,22 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
+import { urlFor } from '@/lib/client'
 
-export default function HeroBanner() {
+export default function HeroBanner({ heroBanner }) {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='beats-solo'>SMALL TEXT</p>
-        <h3>MID TEXT</h3>
+        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+
         {/* TODO: change to nextjs Image component */}
-        <img src='' alt='headphones' className='hero-banner-image' />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt='headphones'
+          className='hero-banner-image'
+        />
 
         <div>
-          <Link href='/product/ID'>
-            <button type='button'>BUTTON TEXT</button>
+          {/* NOTE: Since the slug is the same as product name, we can navigate to /product/heroBanner.product which is the name of the product used on the banner */}
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
+import { client } from '@/lib/client'
 import { HeroBanner, Product, FooterBanner } from '@/components'
 
-export default function Home() {
+export default function Home({ products, bannerData }) {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner
+        heroBanner={
+          // Get the first object from the array
+          bannerData.length && bannerData[0]
+        }
+      />
+
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  // Fetch all products and banner in the Sanity dataset
+  const products = await client.fetch('*[_type == "product"]')
+  const bannerData = await client.fetch('*[_type == "banner"]')
+
+  return {
+    props: {
+      products,
+      bannerData,
+    },
+  }
 }
